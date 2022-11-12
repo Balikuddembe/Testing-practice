19d54824f06b05d56e43bad9069972b5b9efd4c1
Emmanuel Balikuddembe <balixemma@gmail.com>
2022-11-12 16:32:55 +0300

    Add describe

--- Task 3/calculator.test.js
+++ Task 3/calculator.test.js
@@ -12,5 +12,17 @@ describe('Test for arithmetic operators on two numbers',()=>{
         const subtract = new calculator(num1, num2);
         expect(subtract.subtract()).toBe(-4)
     });
+
+    test('Divide two numbers,num1 and num2', ()=>{
+        const num1 = 4, num2 = 2;
+        const divide = new calculator(num1, num2);
+        expect(divide.divide()).toBe(2)
+    });
+
+    test('Multiply two numbers,num1 and num2', ()=>{
+        const num1 = 1, num2 = 5;
+        const multiply = new calculator(num1, num2);
+        expect(multiply.multiply()).toBe(5)
+    });
 }) 
 
